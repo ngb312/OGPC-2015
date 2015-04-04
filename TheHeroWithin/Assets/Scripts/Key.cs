ad9d7f58e3f769e6a6c06287afe216f0e5c504d3
Neel Balusa <neelbalusa@gmail.com>
2015-04-04 13:01:13 -0700

    Added Key to Rescue Mission

---
+++ TheHeroWithin/Assets/Scripts/Key.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Key : MonoBehaviour {
+	public GameObject GameManager;
+	public GameObject Victim;
+	public GameObject Cage;
+
+	void OnTriggerEnter(Collider co)
+	{
+		if (co.tag == "Key") 
+		{
+			Destroy(co.gameObject);
+			Destroy(Victim);
+			Destroy (Cage);
+			var score = GameManager.GetComponent<Score> ();
+			score.FlagFoundVicitm1();
+		}
+	}
+}
